5c13c932ba59e4c19d03a9ab9209e1f5779a1e40
Taco <bensonyao123@gmail.com>
2024-11-26 17:02:12 +0800

    refactor: changed field name for taskDifficulty.

--- app/src/main/java/com/mobdeve/harvesters/kuboquest/FireStoreReferences.java
+++ app/src/main/java/com/mobdeve/harvesters/kuboquest/FireStoreReferences.java
@@ -3,14 +3,16 @@ package com.mobdeve.harvesters.kuboquest;
 public class FireStoreReferences {
     public final static String USER_COLLECTION = "Users";
     public final static String TASK_COLLECTION = "Tasks";
+    public final static String PLANT_COLLECTION = "Plants";
 
     public final static String EMAIL_FIELD = "email";
     public final static String USERNAME_FIELD = "username";
-    public final static String PASSWORD_FIELD = "password";
+    public final static String TOTALENERGYSAVED_FIELD = "totalEnergySaved";
 
     public final static String TASKNAME_FIELD = "taskName";
     public final static String TASKDESC_FIELD = "taskDesc";
     public final static String TASKSTARTDATE_FIELD = "taskStartDate";
     public final static String TASKFREQUENCY_FIELD = "taskFrequency";
-    public final static String TASKDIFFICULTY_FIELD = "taskDiffulty";
+    public final static String TASKDIFFICULTY_FIELD = "taskDifficulty";
+
 }
